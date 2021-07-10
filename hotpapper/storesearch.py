337c04d0b1d042d7d3bd96306d7ca08ad94b9113
Liberbell <liberbell@gmail.com>
2021-07-10 15:42:03 +0900

    storesearch6

--- hotpapper/storesearch.py
+++ hotpapper/storesearch.py
@@ -1,7 +1,13 @@
 import requests
 
-url = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/'
+URL = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/'
 
 with open('key.txt') as f:
     API_key = f.readline()
 
+params = {
+    'key': API_key,
+    'keyword': '神戸'
+}
+
+res = requests.get(URL, params)
