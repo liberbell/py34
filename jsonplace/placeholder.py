ff00fc5780d93bca0367c18cb5947da668b74d88
Liberbell <liberbell@gmail.com>
2021-07-10 15:17:49 +0900

    placeholder1

--- jsonplace/placeholder.py
+++ jsonplace/placeholder.py
@@ -1,10 +1,16 @@
 import requests
 
 url = 'https://jsonplaceholder.typicode.com/posts/'
-res = requests.get(url)
+# res = requests.get(url)
 
 # print(res.status_code)
 # print(res.json()[:5])
 
-datam = res.json()[0]
-print(datam['title'])
+# datam = res.json()[0]
+# print(datam['title'])
+body = {
+    'id': 5
+}
+
+res = requests.get(url, body)
+print(res.status_code)
