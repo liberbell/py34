086e4c0726058ccf06201713509bb114696c49f8
Liberbell <liberbell@gmail.com>
2021-07-11 13:24:18 +0900

    rakusearch3

--- rakuten/rakusearch.py
+++ rakuten/rakusearch.py
@@ -1,3 +1,8 @@
 import json
 import requests
 import pandas as pd
+
+URL = 'https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706'
+
+with open('key.txt') as f:
+    API_key = f.readline()
