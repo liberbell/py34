0dfafca687445a6ff8c43961ffc917c76af364d8
Liberbell <liberbell@gmail.com>
2021-07-13 20:31:03 +0900

    sheets2

--- gcp/sheets.py
+++ gcp/sheets.py
@@ -1,2 +1,14 @@
 import gspread
+from google.oauth2.service_account import Credentials
 
+scopes = [
+    'https://www.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/drive'
+]
+
+credentials = Credentials.from_service_account_file(
+    'secret.json',
+    scopes=scopes
+)
+
+gc = gspread.authorize(credentials)
