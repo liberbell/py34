1e6770423ff01344df86749bab3d80fc73c546a9
Liberbell <liberbell@gmail.com>
2021-07-14 08:43:16 +0900

    sheets2

--- gcp/sheets.py
+++ gcp/sheets.py
@@ -33,4 +33,6 @@ df = df.drop(df.columns[[0]], axis=1)
 # print(df)
 
 # print(df.pivot_table(index=["所属"], values=["年齢"], aggfunc='mean'))
+# print(df.dtypes)
+df = df.astype({"年齢": int})
 print(df.dtypes)
