10ecb7912a9cbe444d4ab4b6fda1cafd12aa27b4
Liberbell <liberbell@gmail.com>
2021-07-14 09:33:07 +0900

    sheets17

--- gcp/sheets.py
+++ gcp/sheets.py
@@ -55,6 +55,6 @@ value_range = "C3:C8"
 
 fmt = CellFormat(
     backgroundColor = color(38/255, 166/255, 154/255),
-    textFormat = textformat(bold=True, foregroundColor=color(255/255, 255/255, 255/255)),
+    textFormat = textFormat(bold=True, foregroundColor=color(255/255, 255/255, 255/255)),
     horizontalAlignment = "CENTER",
 )
