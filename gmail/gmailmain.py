8936ace7fb06285c5cce1322ac6018cbd2965266
Liberbell <liberbell@gmail.com>
2021-07-15 16:41:37 +0900

    gmailmain2

--- gmail/gmailmain.py
+++ gmail/gmailmain.py
@@ -27,16 +27,5 @@ def main():
 
     service = build('gmail', 'v1', credentials=creds)
 
-    # Call the Gmail API
-    results = service.users().labels().list(userId='me').execute()
-    labels = results.get('labels', [])
-
-    if not labels:
-        print('No labels found.')
-    else:
-        print('Labels:')
-        for label in labels:
-            print(label['name'])
-
 if __name__ == '__main__':
     main()
