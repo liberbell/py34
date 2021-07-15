e01a00ee5c9e09c84064a645f347620c5b5070f6
Liberbell <liberbell@gmail.com>
2021-07-15 16:47:34 +0900

    gmailmain4

--- gmail/gmailmain.py
+++ gmail/gmailmain.py
@@ -19,7 +19,7 @@ def send_message(service, user_id, message):
         message = (service.users().messages().send(userId=user_id, body=message).execute())
         print('Message Id: %s' % message['id'])
         return message
-    except errors.HttpError, error:
+    except errors.HttpError as error:
         print('An error occurred: %s' % error)
 
 def main():
