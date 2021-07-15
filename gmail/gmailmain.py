e9c31a5af329b3446777cff70ec0e08ebb9b0b34
Liberbell <liberbell@gmail.com>
2021-07-15 16:45:48 +0900

    gmailmain4

--- gmail/gmailmain.py
+++ gmail/gmailmain.py
@@ -14,6 +14,14 @@ def create_message(sender, to, subject, message_text):
     message['subject'] = subject
     return {'raw': base64.urlsafe_b64encode(message.as_string())}
 
+def send_message(service, user_id, message):
+    try:
+        message = (service.users().messages().send(userId=user_id, body=message).execute())
+        print('Message Id: %s' % message['id'])
+        return message
+    except errors.HttpError, error:
+        print('An error occurred: %s' % error)
+
 def main():
     creds = None
 
