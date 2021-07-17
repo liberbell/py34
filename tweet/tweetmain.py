b6880a51f294e660577190f6828c58ed4f49bc36
Liberbell <liberbell@gmail.com>
2021-07-17 14:59:17 +0900

    tweetmain1

---
+++ tweet/tweetmain.py
@@ -0,0 +1,12 @@
+
+with open('api_key.txt') as f:
+    API_key = f.readline()
+
+with open('secret.txt') as f:
+    API_secret = f.readline()
+
+with open('atoken.txt') as f:
+    access_token = f.readline()
+
+with open('atokensecret.txt') as f:
+    access_token_secret = f.readline()
