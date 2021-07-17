8efa8c7571db9ad0b07b7dad7993752ada2cd3c0
Liberbell <liberbell@gmail.com>
2021-07-17 15:10:48 +0900

    tweetmain4

--- tweet/tweetmain.py
+++ tweet/tweetmain.py
@@ -4,4 +4,9 @@ import tweepy
 with open('secret.json') as f:
     twitter_keys = json.load(f)
 
-tweepy.OAuthHandler(consumer_key, onsumer_secret)
+print(twitter_keys)
+
+consumer_key = twitter_keys["consumer_key"]
+consumer_secret = twitter_keys["consumer_secret"]
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
