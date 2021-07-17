116bafc6f7e9b73791e8e588838599dec52fe199
Liberbell <liberbell@gmail.com>
2021-07-17 15:13:15 +0900

    tweetmain6

--- tweet/tweetmain.py
+++ tweet/tweetmain.py
@@ -14,3 +14,5 @@ access_token_secret = twitter_keys["access_token_secret"]
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 
 auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+# print(api)
