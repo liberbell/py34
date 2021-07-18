eca9647240a2eb947006a7c74057e36da71797fe
Liberbell <liberbell@gmail.com>
2021-07-18 11:54:32 +0900

    main3

--- virus/main.py
+++ virus/main.py
@@ -3,5 +3,5 @@ import requests
 with open('url.txt') as f:
     URL = f.readline()
 
-res = requests.get(URL)
+res = requests.get(URL, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.1 Safari/605.1.15'})
 print(res.text)
