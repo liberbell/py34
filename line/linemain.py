7e1c0531363df7c45d085bf1c38ea9e7625c09e9
Liberbell <liberbell@gmail.com>
2021-07-19 09:03:48 +0900

    linemain4

--- line/linemain.py
+++ line/linemain.py
@@ -6,4 +6,6 @@ URL = "https://notify-api.line.me/api/notify"
 with open("secret.txt") as f:
     line_token = json.load(f)
 
-print(line_token)
+# print(line_token)
+
+LINE_NOTFY_TOKEN = line_token["LINE_NOTFY_TOKEN"]
