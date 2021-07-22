d8eb0946f22b35d0aaf9582e055830fecebf9928
Liberbell <liberbell@gmail.com>
2021-07-22 11:08:56 +0900

    settings3

--- slack/slackbot-settings.py
+++ slack/slackbot-settings.py
@@ -3,6 +3,7 @@ import json
 with open("secret.json") as f:
     slack_token = json.load(f)
 
+# API_TOKEN = slack_token["SLACK_TOKEN"]
 API_TOKEN = slack_token["SLACK_TOKEN"]
 DEFAULT_REPLY = "This is a default message."
 
