54724f3aacd8a565f018cd2ebe4e40b428af72f4
Liberbell <liberbell@gmail.com>
2021-07-23 22:00:54 +0900

    gcpslack9

--- slackgcp/gcpslack.py
+++ slackgcp/gcpslack.py
@@ -1,22 +1,21 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
+def auth():
+    scopes = [
+        'https://www.googleapis.com/auth/spreadsheets',
+        'https://www.googleapis.com/auth/drive'
+    ]
 
-scopes = [
-    'https://www.googleapis.com/auth/spreadsheets',
-    'https://www.googleapis.com/auth/drive'
-]
+    credentials = Credentials.from_service_account_file(
+        'gcp_secret.json',
+        scopes=scopes
+    )
 
-credentials = Credentials.from_service_account_file(
-    'gcp_secret.json',
-    scopes=scopes
-)
+    gc = gspread.authorize(credentials)
 
-gc = gspread.authorize(credentials)
-# print(gc)
+    SP_SHEET_KEY = '14240_Sv7TzeZVxBi42BtK6ZMtaRK82fYqvOqvR1js4s'
+    SP_SHEET = 'timesheet'
 
-SP_SHEET_KEY = '14240_Sv7TzeZVxBi42BtK6ZMtaRK82fYqvOqvR1js4s'
-SP_SHEET = 'timesheet'
-
-worksheet = gc.open_by_key(SP_SHEET_KEY).worksheet(SP_SHEET)
-print(worksheet)
+    worksheet = gc.open_by_key(SP_SHEET_KEY).worksheet(SP_SHEET)
+    return worksheet
