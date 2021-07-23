ba23c7f51974701a40eaefb99ddf76da13c0882a
Liberbell <liberbell@gmail.com>
2021-07-23 21:49:10 +0900

    gcpslack

---
+++ slackgcp/slackbot_settings.py
@@ -0,0 +1,10 @@
+import json
+
+with open("secret.json") as f:
+    slack_token = json.load(f)
+
+API_TOKEN = slack_token["SLACK_TOKEN"]
+# API_TOKEN = "abc"
+DEFAULT_REPLY = "This is a default message."
+
+PLUGINS = ["plugins"]
