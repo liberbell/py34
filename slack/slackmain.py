286f218a78f75d3e2108ac2e6aeda12f5ce3db45
Liberbell <liberbell@gmail.com>
2021-07-23 20:59:56 +0900

    slackmain1

--- slack/slackmain.py
+++ slack/slackmain.py
@@ -4,9 +4,11 @@ from slackbot.bot import Bot
 from slackbot.bot import respond_to
 from slackbot.bot import listen_to
 
-# bot = Bot()
+bot = Bot()
 # bot.run()
 @respond_to('practice')
 
 def sample(message):
     message.send("This is a practie.")
+
+bot.run()
