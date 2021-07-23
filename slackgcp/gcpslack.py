2382f925063563a3062b40b4543377e447890e0e
Liberbell <liberbell@gmail.com>
2021-07-23 21:39:01 +0900

    gcpslack1

---
+++ slackgcp/gcpslack.py
@@ -0,0 +1,5 @@
+import gspread
+from gspread_dataframe import set_with_dataframe
+from gspread_formatting import *
+from google.oauth2.service_account import Credentials
+from gspread_formatting.batch_update_requests import format_cell_range
