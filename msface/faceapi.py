267fdbf68648b557e7ee281dbb69cece9e2c1357
Liberbell <liberbell@gmail.com>
2021-07-26 08:36:09 +0900

    faceapi4

--- msface/faceapi.py
+++ msface/faceapi.py
@@ -19,5 +19,40 @@ with open("faceapi_secret.json") as f:
 KEY = face_api_key["key"]
 ENDPOINT = face_api_key["URL"]
 
-faceclient = FaceClient(ENDPOINT, CognitiveServicesCredentials(KEY))
-print(faceclient)
+face_client = FaceClient(ENDPOINT, CognitiveServicesCredentials(KEY))
+# print(face_client)
+
+# Detect a face in an image that contains a single face
+single_face_image_url = 'https://raw.githubusercontent.com/Microsoft/Cognitive-Face-Windows/master/Data/detection1.jpg'
+single_image_name = os.path.basename(single_face_image_url)
+# We use detection model 3 to get better performance.
+detected_faces = face_client.face.detect_with_url(url=single_face_image_url, detection_model='detection_03')
+if not detected_faces:
+    raise Exception('No face detected from image {}'.format(single_image_name))
+
+# Convert width height to a point in a rectangle
+def getRectangle(faceDictionary):
+    rect = faceDictionary.face_rectangle
+    left = rect.left
+    top = rect.top
+    right = left + rect.width
+    bottom = top + rect.height
+    
+    return ((left, top), (right, bottom))
+
+def drawFaceRectangles() :
+# Download the image from the url
+    response = requests.get(single_face_image_url)
+    img = Image.open(BytesIO(response.content))
+
+# For each face returned use the face rectangle and draw a red box.
+    print('Drawing rectangle around face... see popup for results.')
+    draw = ImageDraw.Draw(img)
+    for face in detected_faces:
+        draw.rectangle(getRectangle(face), outline='red')
+
+# Display the image in the default image browser.
+    img.show()
+
+# Uncomment this to show the face rectangles.
+#    drawFaceRectangles()
