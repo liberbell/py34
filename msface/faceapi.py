e0f7fa077c90a950446a6cb7ea3aa0804bf46761
Liberbell <liberbell@gmail.com>
2021-07-25 22:00:38 +0900

    faceapi2

--- msface/faceapi.py
+++ msface/faceapi.py
@@ -6,9 +6,15 @@ import sys
 import time
 import uuid
 import requests
+import json
 from urllib.parse import urlparse
 from io import BytesIO
 from PIL import Image, ImageDraw
 from azure.cognitiveservices.vision.face import FaceClient
 from msrest.authentication import CognitiveServicesCredentials
 from azure.cognitiveservices.vision.face.models import TrainingStatusType, Person
+
+with open("faceapi_secret.json") as f:
+    face_api_key = json.load(f)
+
+KEY = face_api_key["key"]
