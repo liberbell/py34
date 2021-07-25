d459d6dfe1369b0e8dcf236d294a187988b79071
Liberbell <liberbell@gmail.com>
2021-07-25 09:58:30 +0900

    gcpslack2

--- slackgcp/gcpslack.py
+++ slackgcp/gcpslack.py
@@ -36,7 +36,7 @@ def start_time(message):
     timestamp = datetime.now()
     date = timestamp.strftime('%Y/%m/%d')
     p_time = timestamp.strftime('%H:%M')
-    message.send("Start time is{p_time}. Work hard.")
+    message.send(f"Start time is{p_time}. Work hard.")
     df = df.append({'date': date, 'start time': p_time, 'out time': '00:00'}, ignore_index=True)
 
     worksheet.update([df.columns.tolist()] + df.values.tolist())
