fa53c09d251cc7bc7a72898a21e53fda682eb386
Liberbell <liberbell@gmail.com>
2021-07-26 09:15:49 +0900

    faceapi11

--- msface/faceapi.py
+++ msface/faceapi.py
@@ -25,7 +25,7 @@ single_face_image_url = 'https://raw.githubusercontent.com/Microsoft/Cognitive-F
 single_image_name = os.path.basename(single_face_image_url)
 # detected_faces = face_client.face.detect_with_url(url=single_face_image_url, detection_model='detection_03')
 
-image = Image.open("single.jpg")
+image = open("single.jpg", "rb")
 # single_image_name = os.path.basename(image)
 detected_faces = face_client.face.detect_with_stream(image, detection_model='detection_03')
 
