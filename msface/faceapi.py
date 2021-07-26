5d50998ef99e4d869bfbf4576bbba732602e7e32
Liberbell <liberbell@gmail.com>
2021-07-26 09:16:44 +0900

    faceapi12

--- msface/faceapi.py
+++ msface/faceapi.py
@@ -21,16 +21,16 @@ ENDPOINT = face_api_key["URL"]
 
 face_client = FaceClient(ENDPOINT, CognitiveServicesCredentials(KEY))
 
-single_face_image_url = 'https://raw.githubusercontent.com/Microsoft/Cognitive-Face-Windows/master/Data/detection1.jpg'
-single_image_name = os.path.basename(single_face_image_url)
+# single_face_image_url = 'https://raw.githubusercontent.com/Microsoft/Cognitive-Face-Windows/master/Data/detection1.jpg'
+# single_image_name = os.path.basename(single_face_image_url)
 # detected_faces = face_client.face.detect_with_url(url=single_face_image_url, detection_model='detection_03')
 
 image = open("single.jpg", "rb")
 # single_image_name = os.path.basename(image)
 detected_faces = face_client.face.detect_with_stream(image, detection_model='detection_03')
 
-if not detected_faces:
-    raise Exception('No face detected from image {}'.format(single_image_name))
+# if not detected_faces:
+#     raise Exception('No face detected from image {}'.format(single_image_name))
 
 def getRectangle(faceDictionary):
     rect = faceDictionary.face_rectangle
@@ -41,7 +41,7 @@ def getRectangle(faceDictionary):
     
     return ((left, top), (right, bottom))
 
-response = requests.get(single_face_image_url)
+response = requests.get(image)
 img = Image.open(BytesIO(response.content))
 print('Drawing rectangle around face... see popup for results.')
 draw = ImageDraw.Draw(img)
