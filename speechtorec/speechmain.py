564da9db815793a483e0c994a200fdd236597520
Liberbell <liberbell@gmail.com>
2021-07-27 09:43:52 +0900

    speechmain3

--- speechtorec/speechmain.py
+++ speechtorec/speechmain.py
@@ -1,4 +1,24 @@
 import os
 from google.cloud import speech
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "seech_secret.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "speech_secret.json"
+
+# Instantiates a client
+client = speech.SpeechClient()
+
+# The name of the audio file to transcribe
+gcs_uri = "gs://cloud-samples-data/speech/brooklyn_bridge.raw"
+
+audio = speech.RecognitionAudio(uri=gcs_uri)
+
+config = speech.RecognitionConfig(
+    encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+    sample_rate_hertz=16000,
+    language_code="en-US",
+)
+
+# Detects speech in the audio file
+response = client.recognize(config=config, audio=audio)
+
+for result in response.results:
+    print("Transcript: {}".format(result.alternatives[0].transcript))
