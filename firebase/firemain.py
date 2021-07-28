d02aae76a5bba5feca9c11797cdb99c660d65546
Liberbell <liberbell@gmail.com>
2021-07-28 09:18:43 +0900

    firemain5

--- firebase/firemain.py
+++ firebase/firemain.py
@@ -1,6 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import pandas as pd
 
 # Use a service account
 cred = credentials.Certificate('firebase_secret.json')
@@ -12,7 +13,7 @@ db = firestore.client()
 users_ref = db.collection('users')
 docs = users_ref.stream()
 
+users = []
 for doc in docs:
     print(f'{doc.id} => {doc.to_dict()}')
 
-print(list(docs))
