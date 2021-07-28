36bd5851afc6e2b289f42c52bc821621ba2b9e5f
Liberbell <liberbell@gmail.com>
2021-07-28 09:13:43 +0900

    firemain3

--- firebase/firemain.py
+++ firebase/firemain.py
@@ -7,4 +7,10 @@ cred = credentials.Certificate('firebase_secret.json')
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
-print(db)
+# print(db)
+
+users_ref = db.collection('users')
+docs = users_ref.stream()
+
+for doc in docs:
+    print(f'{doc.id} => {doc.to_dict()}')
