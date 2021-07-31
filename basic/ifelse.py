913e67a03c43c4a776aeafb976a04ee456455b47
Liberbell <liberbell@gmail.com>
2021-07-31 18:23:47 +0900

    ifelse5

--- basic/ifelse.py
+++ basic/ifelse.py
@@ -1,6 +1,8 @@
-money = 100
+money = 900
 
-if money >= 200:
+if money >= 800:
+    print("You can go to the cinema.")
+elif money >= 200:
     print("You can go to the cinema.")
 else:
     print("Stay home.")
