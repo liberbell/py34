987c70dc69ae8380f7b57b3d1584eee132b07f41
Liberbell <liberbell@gmail.com>
2021-08-01 14:12:35 +0900

    adforloops7

--- basic/adforloops.py
+++ basic/adforloops.py
@@ -7,3 +7,9 @@ for last_name, first_name in zip(last_names, first_names):
     full_names.append(full_name)
 
 print(full_names)
+
+for i in range(len(last_names)):
+    print(last_names[i] + " " + first_names[i])
+
+# for index, last_name in enumerate(last_names):
+#     print("ID is ", index, )
