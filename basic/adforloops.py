d91238284778b15f06ca245a24d4e42d498c5f70
Liberbell <liberbell@gmail.com>
2021-08-01 14:08:27 +0900

    adforloops7

--- basic/adforloops.py
+++ basic/adforloops.py
@@ -1,5 +1,5 @@
 last_names = ["elton", "bob", "jhon", "george", "eric"]
-first_names = ["jhon", "mary", "lenon", "harison", "clapton"]
+first_names = ["jhon", "marley", "lennon", "harrison", "clapton"]
 
 full_names = []
 for last_name, first_name in zip(last_names, first_names):
