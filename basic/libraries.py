58c0fbec180a7e7c0033a3d23130d92775be7556
Liberbell <liberbell@gmail.com>
2021-08-04 08:29:18 +0900

    libraries1

--- basic/libraries.py
+++ basic/libraries.py
@@ -1,4 +1,5 @@
 import datetime
+import pandas as pd
 
 today = datetime.date.today()
 print(today.month)
@@ -6,3 +7,9 @@ print(datetime.datetime.now())
 
 print(today - datetime.timedelta(days=1))
 print(today.strftime("%Y年%m月%d日"))
+
+pd.DataFrame([
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]
+            ])
