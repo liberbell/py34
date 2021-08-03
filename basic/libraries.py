91dd9fad27a79e7fc78451a64f55d149122ea952
Liberbell <liberbell@gmail.com>
2021-08-04 08:39:02 +0900

    libraries2

--- basic/libraries.py
+++ basic/libraries.py
@@ -8,8 +8,13 @@ print(datetime.datetime.now())
 print(today - datetime.timedelta(days=1))
 print(today.strftime("%Y年%m月%d日"))
 
-pd.DataFrame([
+df = pd.DataFrame([
             [1, 2, 3],
             [4, 5, 6],
             [7, 8, 9]
             ])
+
+print(df)
+
+df = pd.read_csv("housing.csv")
+print(df)
